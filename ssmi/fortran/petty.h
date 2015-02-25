b7892abce3e112581185b8b4db22c43c686c2c2e
Phil Austin <paustin@eos.ubc.ca>
2015-02-25 14:46:33 -0800

    prune

---
+++ ssmi/fortran/petty.h
@@ -0,0 +1,3 @@
+void emiss_(int *ifreq,float *speed,float *sst,float *theta, float *emissh, float *emissv);
+void coef_(float *sst,float *kl19,float *kl37, float *kv19, float *kv37,float *tox19, float *tox37);
+
