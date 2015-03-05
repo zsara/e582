527b907ff083516929f79fc4229e8916dc9ebf7b
Phil Austin <paustin@eos.ubc.ca>
2015-03-05 12:16:08 -0800

    fix setup

--- cloudmask/setup.py
+++ cloudmask/setup.py
@@ -7,8 +7,8 @@ ext_modules = [Extension('bitmap', ['bitmap.pyx','bitmask.cpp'],
                        include_dirs = [numpy.get_include(),'.'],
                        extra_compile_args=['-O3', '-fPIC'],
                        library_dirs=['.'],
-                       language="c++",
-                       extra_objects=["bitmask.o"])]
+                       language="c++")]
+                       #extra_objects=["bitmask.o"])]
 
 setup(name        = 'bitmap',
       cmdclass    = {'build_ext': build_ext},
